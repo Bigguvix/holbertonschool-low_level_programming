420f26b37022f7119e07e7cd1dc7047943661e5c
Bigguvix <nahufeola@hotmail.com>
2023-10-10 09:54:37 -0700

    Fixed betty for code

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,19 +2,24 @@
 #include <stdio.h>
 #include <time.h>
 
+/**
+ * main - Functio that picks a randon number positive or negative.
+ *
+ * Return: Nothing.
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if(n > 0)
-		printf("%d is positive\n", n); 
-	else if(n == 0)
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
 		printf("%d is zero)", n);
 	else
 		printf("%d is negative", n);
-	
+
 	return (0);
 }
