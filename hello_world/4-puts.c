db00ca2fe36979b88334d46252f507d1aab6d9ad
Bigguvix <nahufeola@hotmail.com>
2023-10-10 08:25:56 -0700

    My first program

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints a line using puts
+ *
+ * Return: Nothing.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
