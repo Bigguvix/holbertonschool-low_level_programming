bf5334efd2f97377ad39f45f8dfd691a50dca332
Bigguvix <nahufeola@hotmail.com>
2023-10-10 08:58:21 -0700

    Fixed betty style and variables error

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Main Function, prints sizes of various types.
+ *
+ *
+ * Return: Nothing.
+ */
 int main(void)
 {
 	char a;
+	int v;
 	long int b;
 	long long int c;
 	float d;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(a));
-	printf("Size of an int: %zu byte(s)\n", sizeof(b));
+	printf("Size of an int: %zu byte(s)\n", sizeof(v));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(b));
-	printf("Size of a long long it: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(c));
 	printf("Size of a float: %zu byte(s)\n", sizeof(d));
-	return(0);
+	return (0);
 }
