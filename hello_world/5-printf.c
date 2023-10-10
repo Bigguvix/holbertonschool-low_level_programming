e33bc8cedaf90e3917b7ffaa012d858601cd1aba
Bigguvix <nahufeola@hotmail.com>
2023-10-10 08:31:04 -0700

    Prints a line using printf function

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints a line using printf
+ *
+ * Return: Nothing.
+ */
+int main(void)
+{
+	printf("%s \n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
