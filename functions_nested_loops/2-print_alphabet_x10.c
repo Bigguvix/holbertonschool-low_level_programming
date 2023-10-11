f665d67e478cb98d20d1dc66a9ea2f9216bd3b1d
Bigguvix <nahufeola@hotmail.com>
2023-10-11 09:47:08 -0700

    Prints alphabet by 10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int y;
+	char j;
+	
+	y = 0;	
+
+	while (y <= 10)
+	{	
+	for (j = 'a'; j <= 'z'; ++j)
+		_putchar(j);
+	_putchar('\n');
+	y++;
+	}
+}
