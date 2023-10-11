c83505621f4940aaa8270865eb01fdfb4c947f00
Bigguvix <nahufeola@hotmail.com>
2023-10-11 07:18:19 -0700

    Alphabet upper program

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ *
+ * Return: nothing.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'A'; c <= 'Z'; ++c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
