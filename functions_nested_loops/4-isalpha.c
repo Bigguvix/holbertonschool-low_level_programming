ffd46646a0fee798e8b2c3758df7e84c533b6694
Bigguvix <nahufeola@hotmail.com>
2023-10-11 10:57:17 -0700

    Fixed code

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -9,14 +9,14 @@
  */
 int _isalpha(int c)
 {
-	char a, b;
+	char a, B;
 	int u = 0;
 
-	for (a = 'a'; a <= 'b'; ++a)
+	for (a = 'a'; a <= 'z'; a++)
 	{
-		for (b = 'A'; b <= 'Z'; ++b)
+		for (B = 'A'; B <= 'Z'; B++)
 		{
-			if (c == a || c == b)
+			if (c == a || c == B)
 				u = 1;
 		}
 	}
