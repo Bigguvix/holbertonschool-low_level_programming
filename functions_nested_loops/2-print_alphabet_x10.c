c7aa5d4f9c8d70295ccc3b68412b155966821152
Bigguvix <nahufeola@hotmail.com>
2023-10-11 10:04:41 -0700

    Fixed betty style

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
+/**
+ * print_aplhabet_x10 - Print the alphabet ten times
+ *
+ * Return: The alphabet ten times.
+ */
 void print_alphabet_x10(void)
 {
 	int y;
 	char j;
-	
-	y = 0;	
+
+	y = 0;
 
 	while (y < 10)
-	{	
+	{
 	for (j = 'a'; j <= 'z'; ++j)
 		_putchar(j);
 	_putchar('\n');
