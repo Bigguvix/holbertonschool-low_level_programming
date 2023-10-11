92a7ffc01c14da944c9884a4c348ca12fce71b85
Bigguvix <nahufeola@hotmail.com>
2023-10-11 09:27:30 -0700

    Alphabet in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase.
+ *
+ * Return: The alphabet in lowercase.
+ */
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; ++j)
+		_putchar(j);
+	_putchar('\n');
+}
