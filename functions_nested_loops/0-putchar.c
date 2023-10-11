cb1ea780eb2eb0cbaf7e7418e04d753e3d122b85
Bigguvix <nahufeola@hotmail.com>
2023-10-11 09:14:16 -0700

    Functionc that writes _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Main Function.
+ *
+ * Return: Nothing.
+ */
+int main(void)
+{
+	char a[8] = "_putchar";
+	int b;
+
+	for (b = 0; b < 9; ++b)
+		_putchar(a[b]);
+	_putchar('\n');
+
+	return (0);
+}
