d21f9277311d88fc551f90c6fa6a71e0fb6d5da3
Bigguvix <nahufeola@hotmail.com>
2023-10-12 05:31:19 -0700

    Program #6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _abs - Main entry.
+ *
+ * @l: Entry point.
+ *
+ * Return: Nothing.
+ */
+int _abs(int l)
+{
+	if (l >= 0)
+	{
+		return (l);
+	}
+
+	else
+
+	return (l * -1);
+}
