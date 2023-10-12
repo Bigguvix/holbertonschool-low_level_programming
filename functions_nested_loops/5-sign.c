d4d18dfe96f243bb83185c74ee05a8a9283064eb
Bigguvix <nahufeola@hotmail.com>
2023-10-12 04:42:21 -0700

    Sign of numbers

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int print_sign(int n)
+{
+	int a;
+
+
+       if (n > 0)
+       {
+	       a = 1;
+	       _putchar('+');
+       }
+
+       else if (n == 0)
+       {
+	       a = 0;
+	       _putchar('0');
+       }
+
+       else
+       {
+	       a = -1;
+	       _putchar('-');
+       }
+
+       return (a);
+}
