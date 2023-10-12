147405331e517e911ba56869307a29a2aeb338f9
Bigguvix <nahufeola@hotmail.com>
2023-10-12 06:28:07 -0700

    Multiplication tables

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; ++a)
+	{
+		for (b = 0; b <= 9; ++b)
+		{
+		int c = a * b;
+
+		if ((c / 10) == 0)
+		{
+			if (b != 0)
+				_putchar(' ');
+			_putchar(c + '0');
+
+		if (b == 9)
+			continue;
+		_putchar(',');
+		_putchar(' ');
+		}
+
+		else
+		{
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+
+			if (b == 9)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
