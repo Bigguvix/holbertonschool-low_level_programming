4c8584e13ec3fb84e38ea089505fcd88c9e03549
Bigguvix <nahufeola@hotmail.com>
2023-10-12 04:48:03 -0700

    Fixed betty style

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,27 +1,34 @@
 #include "main.h"
 
+/**
+ * print_sign - Main Function.
+ *
+ *
+ * @n: an Entry point.
+ *
+ * Return: Nothing.
+ */
 int print_sign(int n)
 {
 	int a;
 
+	if (n > 0)
+	{
+		a = 1;
+		_putchar('+');
+	}
 
-       if (n > 0)
-       {
-	       a = 1;
-	       _putchar('+');
-       }
+	else if (n == 0)
+	{
+		a = 0;
+		_putchar('0');
+	}
 
-       else if (n == 0)
-       {
-	       a = 0;
-	       _putchar('0');
-       }
+	else
+	{
+		a = -1;
+		_putchar('-');
+	}
 
-       else
-       {
-	       a = -1;
-	       _putchar('-');
-       }
-
-       return (a);
+	return (a);
 }
