7ac397cd7cb9b9a4c3a3f2fc47cfbb2865978516
Bigguvix <nahufeola@hotmail.com>
2023-10-17 07:45:35 -0700

    Product of 2 iteger

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Program to do the product of 2 integers.
+ *
+ * @a: Integer.
+ * @b: Integer.
+ *
+ * Return: The product of a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
