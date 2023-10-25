1f885f63327dbc04ba425ef41f562bcab203aea7
Bigguvix <nahufeola@hotmail.com>
2023-10-25 07:11:27 -0700

    #6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Main Function
+ *
+ * @str: Character string
+ *
+ * Return: Nothing.
+ */
+void puts2(char *str)
+{
+	int lar = 0;
+	int a = 0;
+
+	while (str[lar] != '\0')
+		++lar;
+
+	lar -= 1;
+
+	for (; a <= lar; a += 2)
+		putchar(str[a]);
+
+	putchar('\n');
+}
