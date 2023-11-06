9143a540aa8644be41c935ec8bdf9d4b415c59c4
Bigguvix <nahufeola@hotmail.com>
2023-11-06 07:45:17 -0800

    #1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - Main Function
+ *
+ * @d: Structure named dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ *
+ * Return: nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
