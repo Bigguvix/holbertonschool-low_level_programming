91f4eeb45b9b8aa3c5d4e69b6595000df48c1410
Bigguvix <nahufeola@hotmail.com>
2023-11-06 08:03:08 -0800

    fixed betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,9 +17,11 @@ struct dog
 };
 
 /**
- * __STRUCTURES__
+ *init_dog - Main Functiion.
+ *@d: struct.
+ *@name: name.
+ *@age: age.
+ *@owner: owner
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 #endif
