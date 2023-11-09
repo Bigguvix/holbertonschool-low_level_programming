0a3ae9560b942aecf5b49f70b68bf3c10c570062
Bigguvix <nahufeola@hotmail.com>
2023-11-09 07:07:20 -0800

    #fixed

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,86 +1,51 @@
-#include "dog.h"
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
-
-int _len(char *str);
-char *_copy(char *dest, char *src);
-dog_t *new_dog(char *name, float age, char *owner);
 
 /**
- * _len - This is a function to find the length of a string.
+ * new_dog - Main function.
  *
- * @str: This is the string to be measured.
- *
- * Return: The measure of the string.
- */
-int _len(char *str)
-{
-	int lar = 0;
-
-	while (*str++)
-		++lar;
-
-	return (lar);
-}
-
-/**
- * _copy - Copies a string pointed by src to a buffer pointed by dest.
- *
- * @dest: The buffer storing the copy of the string.
- * @src: The string itself.
- *
- * Return: The pointer to dest.
- */
-char *_copy(char *dest, char *src)
-{
-	int i = 0;
-
-	for (i = 0; src[i]; ++i)
-		dest[i] = src[i];
-
-	dest[i] = '\0';
-
-	return (dest);
-}
-
-/**
- * new_dog - Main Function.
- *
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
  *
  * Return: dogg.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dogg;
+	char *cpyn, *cpyo;
+	int larn = 0, laro = 0, a;
 
-	if (name == NULL || age < 0 || owner == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
+
+	while (name[larn])
+		++larn;
+
+	while (owner[laro])
+		++laro;
 
 	dogg = malloc(sizeof(dog_t));
 	if (dogg == NULL)
 		return (NULL);
 
-	dogg->name = malloc(sizeof(char) * (strlen(name) + 1));
-	if (dogg->name == NULL)
-	{
-		free(dogg);
+	cpyn = malloc(larn + 1);
+	if (cpyn == NULL)
 		return (NULL);
-	}
+	for (a = 0; name[a]; ++a)
+		cpyn[a] = name[a];
+	cpyn[a] = '\0';
 
-	dogg->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-	if (dogg->owner == NULL)
-	{
-		free(dogg->name);
-		free(dogg);
+	cpyo = malloc(laro + 1);
+	if (cpyo == NULL)
 		return (NULL);
-	}
+	for (a = 0; name[a]; ++a)
+		cpyo[a] = owner[a];
+	cpyo[a] = '\0';
 
-	dogg->name = strcpy(dogg->name, name);
+	dogg->name = cpyn;
 	dogg->age = age;
-	dogg->owner = strcpy(dogg->owner, owner);
+	dogg->owner = cpyo;
 
 	return (dogg);
 }
