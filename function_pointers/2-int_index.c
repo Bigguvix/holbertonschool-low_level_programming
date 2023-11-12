e4559bbdb41ae92e949ea51951a64432459962bd
Bigguvix <nahufeola@hotmail.com>
2023-11-12 15:54:29 -0800

    #2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Main function.
+ *
+ * @array: array.
+ * @size: size of array.
+ * @cmp: pointer to function.
+ *
+ * Return: -1 or -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; ++i)
+	{
+		if (cmp(array[i] != 0)
+				return (i);
+	}
+
+	return (-1);
+}
