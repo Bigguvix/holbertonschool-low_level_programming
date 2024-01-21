e1a4bc17b257b40d34444603c76acdfb3bac425a
Bigguvix <nahufeola@hotmail.com>
2024-01-21 07:14:07 -0800

    Prototypes

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/** __PROTOTYPES__ */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
